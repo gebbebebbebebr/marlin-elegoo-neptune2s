9632570653a76a8c8dcebfe3f32e267b3f571b88
unknown <>
2023-03-10 00:37:03 +0100

    fixes

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1695,7 +1695,7 @@
 #endif
 //#define Z_AFTER_PROBING           5 // Z position after probing is done
 
-#define Z_PROBE_LOW_POINT          -3 // Farthest distance below the trigger-point to go before stopping
+#define Z_PROBE_LOW_POINT          -5 // Farthest distance below the trigger-point to go before stopping
 
 // For M851 give a range for adjusting the Z probe offset
 #define Z_PROBE_OFFSET_RANGE_MIN -20
